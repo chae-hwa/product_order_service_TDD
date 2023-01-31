d7a55a2888affdb5d80c528ff24e0e7b99384ef5
chae-hwa <ihj0519@naver.com>
2023-01-31 14:11:29 +0900

    상품 등록 API 개발 : 스프링부트 테스트로 전환

--- src/main/java/com/example/product_order_service/product/ProductAdapter.java
+++ src/main/java/com/example/product_order_service/product/ProductAdapter.java
@@ -1,5 +1,8 @@
 package com.example.product_order_service.product;
 
+import org.springframework.stereotype.Component;
+
+@Component
 class ProductAdapter implements ProductPort {
     private final ProductRepository productRepository;
 
--- src/test/java/com/example/product_order_service/product/ProductServiceTest.java
+++ src/test/java/com/example/product_order_service/product/ProductServiceTest.java
@@ -3,9 +3,12 @@ package com.example.product_order_service.product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class ProductServiceTest {
-
+    @Autowired
     private ProductService productService;
     private ProductPort productPort;
     private ProductRepository productRepository;
