c9ac9dad492bda884fa974eea1439689bd0a9f62
chae-hwa <ihj0519@naver.com>
2023-02-01 15:11:23 +0900

    상품 조회 API 개발 : 상품 조회 기능 구현

---
+++ src/main/java/com/example/product_order_service/product/GetProductResponse.java
@@ -0,0 +1,12 @@
+package com.example.product_order_service.product;
+
+import static org.springframework.util.Assert.hasText;
+import static org.springframework.util.Assert.notNull;
+
+record GetProductResponse(long id, String name, int price, DiscountPolicy discountPolicy) {
+    public GetProductResponse {
+        notNull(id, "상품 ID는 필수입니다.");
+        hasText(name, "상품명은 필수입니다.");
+        notNull(discountPolicy, "할인 정책은 필수입니다.");
+    }
+}
