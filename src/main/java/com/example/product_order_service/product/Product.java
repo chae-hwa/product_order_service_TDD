dff7bee86581363f309ae0956661f95556f5c008
chae-hwa <ihj0519@naver.com>
2023-02-01 23:01:52 +0900

    상품 수정 API 개발 : POJO 상품 수정 기능 구현

--- src/main/java/com/example/product_order_service/product/Product.java
+++ src/main/java/com/example/product_order_service/product/Product.java
@@ -29,4 +29,13 @@ class Product {
         this.price = price;
         this.discountPolicy = discountPolicy;
     }
+
+    public void update(final String name, final int price, final DiscountPolicy discountPolicy) {
+        hasText(name, "상품명은 필수입니다.");
+        isTrue(price > 0, "상품 가격은 0보다 커야 합니다.");
+        notNull(discountPolicy, "할인 정책은 필수입니다.");
+        this.name = name;
+        this.price = price;
+        this.discountPolicy = discountPolicy;
+    }
 }
---
+++ src/main/java/com/example/product_order_service/product/UpdateProductRequest.java
@@ -0,0 +1,11 @@
+package com.example.product_order_service.product;
+
+import static org.springframework.util.Assert.*;
+
+record UpdateProductRequest(String name, int price, DiscountPolicy discountPolicy) {
+    UpdateProductRequest {
+        hasText(name, "상품명은 필수입니다.");
+        isTrue(price > 0, "상품 가격은 0보다 커야 합니다.");
+        notNull(discountPolicy, "할인 정책은 필수입니다.");
+    }
+}
