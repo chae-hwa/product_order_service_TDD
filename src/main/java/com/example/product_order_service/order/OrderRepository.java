7af010d8b61439e7a5737b91a28b8cbad9f616ab
chae-hwa <ihj0519@naver.com>
2023-02-02 22:40:14 +0900

    주문 결제 API 개발 : JPA 적용

--- src/main/java/com/example/product_order_service/order/OrderRepository.java
+++ src/main/java/com/example/product_order_service/order/OrderRepository.java
@@ -2,6 +2,6 @@ package com.example.product_order_service.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
--- src/main/java/com/example/product_order_service/payment/Payment.java
+++ src/main/java/com/example/product_order_service/payment/Payment.java
@@ -1,12 +1,26 @@
 package com.example.product_order_service.payment;
 
 import com.example.product_order_service.order.Order;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "payment")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final Order order;
-    private final String cardNumber;
+
+    @OneToOne
+    private Order order;
+    private String cardNumber;
 
     public Payment(final Order order, final String cardNumber) {
         Assert.notNull(order, "주문은 필수입니다.");
@@ -15,19 +29,7 @@ class Payment {
         this.cardNumber = cardNumber;
     }
 
-    public void assignId(final Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
     public int getPrice() {
         return order.getTotalPrice();
-    }
-
-    public String getCardNumber() {
-        return cardNumber;
     }
 }
